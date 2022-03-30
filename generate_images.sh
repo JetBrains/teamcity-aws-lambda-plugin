88e98a3e4c23f4878dcb040543be868dc23784d3
Andre Rocha <andre.rocha@jetbrains.com>
2022-03-30 15:57:47 +0100

    Add maven-specific image

---
+++ generate_images.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+generate_image(){
+  image_name=$1
+  images_folder="images"
+
+  mkdir -p "$images_folder/build"
+  cat "$images_folder/Dockerfile-common" "$images_folder/Dockerfile-$image_name" > "$images_folder/build/Dockerfile-$image_name"
+}
+
+
+generate_image gradle
+generate_image maven
